52f8aeb094696778ce5808a7a3eee0d960110060
benjidurden <benjidurdenn@gmail.com>
2017-10-07 10:29:42 -0400

    fixed schema

--- models/carts.sql
+++ models/carts.sql
@@ -3,14 +3,23 @@ CREATE DATABASE kinnes_carts;
 
 \c kinnes_carts;
 
+CREATE TABLE userInfo (
+    userName TEXT PRIMARY KEY
+);
+
 CREATE TABLE userCart (
     ID SERIAL PRIMARY KEY,
-    cartOwnerName TEXT,
+    -- The cartOwnerName has a foreign key linked to the userInfo table's userName column - I just forgot to put it in the schema! My fault.
+    cartOwnerName TEXT, 
     userItems TEXT,
     amountOfPrints INTEGER,
     amountOfShirts INTEGER,
     shirtSize TEXT
 );
 
+ALTER TABLE userCart ADD FOREIGN KEY (cartOwnerName) REFERENCES userInfo (userName);
+
 INSERT INTO userCart (cartOwnerName, userItems, amountofPrints, amountofShirts, shirtSize)
 VALUES('Francis', 'Bat', 2, 2, 'Small');
+
+
